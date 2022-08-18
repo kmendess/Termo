57f099cab5bf86109e55871276a5cd699f4395ea
kmendess <karolmendes.rj@gmail.com>
2022-08-17 23:59:58 -0300

    Added Interfaces to Domain

---
+++ API/Termo.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+using Termo.Domain.Models;
+
+namespace Termo.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : BaseEntity
+    {
+        Task<TEntity> GetById(Guid id);
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> where);
+        Task<TEntity> Add(TEntity entity);
+        Task<TEntity> Update(TEntity entity);
+        Task Delete(Guid id);
+    }
+}
---
+++ API/Termo.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Termo.Domain.Models;
+
+namespace Termo.Domain.Interfaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+    }
+}
